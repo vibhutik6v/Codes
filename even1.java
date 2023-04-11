d7c32f72a267eb21f813b4a2f12fd9c380770fd4
vibhutik6v <114976379+vibhutik6v@users.noreply.github.com>
2023-04-11 06:59:41 +0530

    Add files via upload

---
+++ even1.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+
+public class even1 {
+    public static void main(String[]args){
+        int num=5;
+        if(num%2==0){
+       // if((num|1)>num){
+       
+            System.out.println("even");
+
+         }else{
+            System.out.println("odd");
+    }
+}
+}
