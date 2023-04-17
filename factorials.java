e238a529a6bb25891c85bf8c6d167e3ac5eb71e2
vibhutik6v <114976379+vibhutik6v@users.noreply.github.com>
2023-04-18 05:18:32 +0530

    Add files via upload

---
+++ factorials.java
@@ -0,0 +1,19 @@
+public class factorials {
+    
+  
+static int factorial(int n)
+    {
+ 
+        if (n == 0 || n == 1)
+            return 1;
+ 
+        return n * factorial(n - 1);
+    }
+ 
+    public static void main(String[] args)
+    {
+        int ans = factorial(5);
+ 
+        System.out.println( ans);
+    }
+}
